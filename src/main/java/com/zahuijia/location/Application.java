837a72daf15341761d91f628a07858090ff769d8
unknown <472694060@qq.com>
2018-10-31 16:08:34 +0800

    提交hello
    Co-Authored-By: lincolngithub <472694060@qq.com>

--- src/main/java/com/zahuijia/location/Application.java
+++ src/main/java/com/zahuijia/location/Application.java
@@ -9,5 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        System.out.println(" --- start --- ");
     }
 }
